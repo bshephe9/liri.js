972efe69f82df5a204c2bb5b55d7a55bce676b96
bshephe9 <brookeshepherd672@gmail.com>
2018-10-26 23:08:03 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,7 +1,13 @@
-const dotenv = require("dotenv").config();
+require("dotenv").config();
+
 const keys = require ('./keys.js');
-
-var spotify = new Spotify(keys.spotify);
-
+const fs = require ("fs");
+var request = require("request");
+//require Spotify API 
+var Spotify = require('node-spotify-api');
+//file for logging results
+var filename = './log.text';
+//argv [2] chooses users actions, argv [3] input parameter
 const command = process.argv[2];
 const secondCommand = process.argv[3];
+
